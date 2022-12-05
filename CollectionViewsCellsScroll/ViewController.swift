dc89cf5ffcd07e0f5e04b26943aa36f8751f7e87
Felipe Vieira <felipe.lima@bancointer.com.br>
2022-12-05 12:22:59 -0300

    Initial Commit

---
+++ CollectionViewsCellsScroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewsCellsScroll
+//
+//  Created by Felipe Vieira Lima on 05/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
